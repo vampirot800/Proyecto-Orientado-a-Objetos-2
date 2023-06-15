dbd883ba1a3098020c417288cca6210a74588569
vampirot800 <111137463+vampirot800@users.noreply.github.com>
2023-06-15 02:08:26 -0600

    Comentarios

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+            // case 1:
+            //     pi.addSolidFuel(1);
+            //     break;
+            // case 2:
+            //     pi.addSolidFuel(2);
+            //     break;
+            // case 3:
+            //     pi.addLiquidFuel();
+            //     break;
+            // case 4:
+            //     pi.addIonFuel();
+            //     break;
+            // };
